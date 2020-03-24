3894e9364274271ddffc6d67f771636eab526f74
Bruno Cruz <bru.cruz@gmail.com>
2020-03-23 21:31:07 -0300

    concluído o desafio dos conceitos

--- bundle.js
+++ bundle.js
@@ -1,25 +1,251 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-alert('Teste');
+var Usuario = /*#__PURE__*/function () {
+  function Usuario(email, senha) {
+    _classCallCheck(this, Usuario);
 
-var Teste = /*#__PURE__*/function () {
-  function Teste() {
-    _classCallCheck(this, Teste);
+    this.email = email;
+    this.senha = senha;
+    this.admin = false;
   }
 
-  _createClass(Teste, [{
-    key: "metodo",
-    value: function metodo() {}
-  }, {
-    key: "outro",
-    value: function outro() {}
+  _createClass(Usuario, [{
+    key: "isAdmin",
+    value: function isAdmin() {
+      console.log(this.admin === true);
+    }
   }]);
 
-  return Teste;
+  return Usuario;
 }();
+
+var Admin = /*#__PURE__*/function (_Usuario) {
+  _inherits(Admin, _Usuario);
+
+  var _super = _createSuper(Admin);
+
+  function Admin() {
+    var _this;
+
+    _classCallCheck(this, Admin);
+
+    _this = _super.call(this);
+    _this.admin = true;
+    return _this;
+  }
+
+  return Admin;
+}(Usuario);
+
+var User1 = new Usuario('email@teste.com', 'senha123');
+var Adm1 = new Admin('email@teste.com', 'senha123');
+User1.isAdmin(); // false
+
+Adm1.isAdmin(); // true
+
+var usuarios = [{
+  nome: 'Diego',
+  idade: 23,
+  empresa: 'Rocketseat'
+}, {
+  nome: 'Gabriel',
+  idade: 15,
+  empresa: 'Rocketseat'
+}, {
+  nome: 'Lucas',
+  idade: 30,
+  empresa: 'Facebook'
+}];
+var map = usuarios.map(function (item) {
+  return item.idade;
+});
+console.log(map);
+var filter = usuarios.filter(function (item) {
+  return item.empresa === 'Rocketseat' && item.idade > 18;
+});
+console.log(filter);
+var find = usuarios.find(function (item) {
+  return item.empresa === 'Google';
+});
+console.log(find);
+var dobroUsuarios = usuarios.map(function (item) {
+  item.idade = item.idade * 2;
+  return item;
+});
+var menos50 = dobroUsuarios.filter(function (item) {
+  return item.idade < 50;
+});
+console.log(menos50); // 3.1
+
+var arr = [1, 2, 3, 4, 5];
+arr.map(function (item) {
+  return item + 10;
+});
+console.log(arr); // 3.2
+// Dica: Utilize uma constante pra function
+
+var usuario = {
+  nome: 'Diego',
+  idade: 23
+};
+
+var idade2 = function idade2(usuario) {
+  return usuario.idade;
+};
+
+console.log(idade2(usuario)); // 3.3
+// Dica: Utilize uma constante pra function
+
+var nome = "Diego";
+var idade = 23;
+
+var mostraUsuario = function mostraUsuario() {
+  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
+  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
+  return {
+    nome: nome,
+    idade: idade
+  };
+};
+
+console.log(mostraUsuario(nome, idade));
+console.log(mostraUsuario(nome)); // 3.4
+
+var promise = function promise() {
+  return new Promise(function (resolve, reject) {
+    return resolve();
+  });
+};
+
+console.log(promise());
+
+var promise2 = function promise2() {
+  return new Promise(function (resolve, reject) {
+    return resolve();
+  });
+};
+
+console.log(promise2()); //4o exercício:
+//4.1 Desestruturação simples
+
+var empresa = {
+  nome2: 'Rocketseat',
+  endereco2: {
+    cidade2: 'Rio do Sul',
+    estado2: 'SC'
+  }
+};
+console.log(empresa);
+var nome2 = empresa.nome2,
+    _empresa$endereco = empresa.endereco2,
+    cidade2 = _empresa$endereco.cidade2,
+    estado2 = _empresa$endereco.estado2;
+console.log(nome2); // Rocketseat
+
+console.log(cidade2); // Rio do Sul
+
+console.log(estado2); // SC
+//4.2 Desestruturação em parâmetros
+
+function mostraInfo(_ref) {
+  var nome10 = _ref.nome10,
+      idade10 = _ref.idade10;
+  return "".concat(nome10, " tem ").concat(idade10, " anos.");
+}
+
+console.log(mostraInfo({
+  nome10: 'Diego',
+  idade10: 23
+})); //5o Exercicio
+// 5.1 Rest
+
+var arr2 = [1, 2, 3, 4, 5, 6];
+var x = arr2[0],
+    y = arr2.slice(1);
+console.log(x); // 1
+
+console.log(y); // [2, 3, 4, 5, 6]
+// function soma...
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
+}
+
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+
+console.log(soma(1, 2)); // 3
+//5.2 Spread
+
+var usuario100 = {
+  nome100: 'Diego',
+  idade100: 23,
+  endereco100: {
+    cidade100: 'Rio do Sul',
+    uf100: 'SC',
+    pais100: 'Brasil'
+  }
+};
+console.log(usuario100);
+
+var usuario200 = _objectSpread({}, usuario100, {
+  nome100: 'Gabriel'
+});
+
+console.log(usuario200);
+
+var usuario300 = _objectSpread({}, usuario200, {
+  endereco100: {
+    cidade100: 'Lontras',
+    uf100: 'SC',
+    pais: 'Brasil'
+  }
+});
+
+console.log(usuario300); //6o exercício
+
+var usuario1000 = 'Bruno';
+var idade1000 = 30;
+console.log("O usu\xE1rio ".concat(usuario1000, " possui ").concat(idade1000, " anos")); //7o exercício
+
+var nome10000 = 'Diego';
+var idade10000 = 23;
+var usuario10000 = {
+  nome10000: nome10000,
+  idade10000: idade10000,
+  cidade10000: 'Rio do Sul'
+};
+console.log(usuario10000);
--- main.js
+++ main.js
@@ -1,10 +1,155 @@
-alert('Teste');
-
-class Teste {
-    metodo() {
+class Usuario {
+    constructor(email, senha) {
+        this.email = email;
+        this.senha = senha;
+        this.admin = false;
     }
 
-    outro() {
-        
+    isAdmin() {
+        console.log(this.admin === true);
     }
 }
+
+class Admin extends Usuario {
+    constructor() {
+        super();
+        this.admin = true;
+    }
+}
+
+const User1 = new Usuario('email@teste.com', 'senha123');
+const Adm1 = new Admin('email@teste.com', 'senha123');
+
+User1.isAdmin() // false
+Adm1.isAdmin() // true
+
+const usuarios = [
+    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
+    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
+    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
+   ];
+
+const map = usuarios.map(item => item.idade);
+console.log(map);
+
+const filter = usuarios.filter(item => item.empresa === 'Rocketseat' && item.idade > 18);
+console.log(filter);
+
+const find = usuarios.find(item => item.empresa === 'Google');
+console.log(find);
+
+const dobroUsuarios = usuarios.map(function(item) {
+    item.idade = item.idade*2;
+    return item;
+});
+
+const menos50 = dobroUsuarios.filter(item => item.idade<50);
+console.log(menos50);
+
+
+// 3.1
+const arr = [1, 2, 3, 4, 5];
+arr.map(item => item + 10);
+console.log(arr);
+
+// 3.2
+// Dica: Utilize uma constante pra function
+const usuario = { nome: 'Diego', idade: 23 };
+const idade2 = usuario => usuario.idade;
+console.log(idade2(usuario));
+
+// 3.3
+// Dica: Utilize uma constante pra function
+const nome = "Diego";
+const idade = 23;
+const mostraUsuario = (nome = 'Diego', idade = 18) => ({ nome, idade });
+console.log(mostraUsuario(nome, idade));
+console.log(mostraUsuario(nome));
+
+// 3.4
+const promise = function() {
+    return new Promise(function(resolve, reject) {
+    return resolve();
+    })
+   }
+console.log(promise());
+
+const promise2 = () => new Promise( (resolve, reject) => resolve() );
+console.log(promise2());
+
+//4o exercício:
+//4.1 Desestruturação simples
+
+const empresa = {
+    nome2: 'Rocketseat',
+    endereco2: {
+    cidade2: 'Rio do Sul',
+    estado2: 'SC',
+    }
+   };
+
+console.log(empresa);
+
+const { nome2, endereco2: { cidade2, estado2 } } = empresa;
+
+console.log(nome2); // Rocketseat
+console.log(cidade2); // Rio do Sul
+console.log(estado2); // SC
+
+//4.2 Desestruturação em parâmetros
+
+function mostraInfo( {nome10, idade10}) {
+    return `${nome10} tem ${idade10} anos.`;
+   }
+console.log(mostraInfo({ nome10: 'Diego', idade10: 23 }));
+
+//5o Exercicio
+// 5.1 Rest
+
+const arr2 = [1, 2, 3, 4, 5, 6];
+const [ x, ...y ] = arr2;
+
+console.log(x); // 1
+console.log(y); // [2, 3, 4, 5, 6]
+
+// function soma...
+function soma(...params) {
+    return params.reduce((total, next) => total + next);
+}
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+console.log(soma(1, 2)); // 3
+
+//5.2 Spread
+
+const usuario100 = {
+    nome100: 'Diego',
+    idade100: 23,
+    endereco100: {
+    cidade100: 'Rio do Sul',
+    uf100: 'SC',
+    pais100: 'Brasil',
+    }
+   };
+console.log(usuario100);
+
+const usuario200 = { ...usuario100, nome100: 'Gabriel'};
+console.log(usuario200);
+
+const usuario300 = { ...usuario200, endereco100: {cidade100: 'Lontras', uf100: 'SC', pais: 'Brasil'} };
+console.log(usuario300);
+
+//6o exercício
+const usuario1000 = 'Bruno';
+const idade1000 = 30;
+console.log(`O usuário ${usuario1000} possui ${idade1000} anos`);
+
+//7o exercício
+const nome10000 = 'Diego';
+const idade10000 = 23;
+const usuario10000 = {
+ nome10000,
+ idade10000,
+ cidade10000: 'Rio do Sul',
+};
+console.log(usuario10000)
+
